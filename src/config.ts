2d8b1fcd3dd7c8728900b38f0aaaa0dac8e4cb3d
senghank abpoc1 cloud9 <senghank+abpoc1@amazon.com>
2022-02-16 01:07:36 +0000

    change backend to oragon

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
-    REGION: "us-east-1",
-    API_URL: "https://yuq7ws39pj.execute-api.us-east-1.amazonaws.com/prod",
+    REGION: "us-west-2",
+    API_URL: "https://rbfjg4lk5e.execute-api.us-west-2.amazonaws.com/prod",
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_yA0vYd5AL",
-    APP_CLIENT_ID: "25048pu470diqqcvdo56i0cat9",
-    IDENTITY_POOL_ID: "us-east-1:1ff5a5f9-dd44-4934-b575-97d8d0952439"
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_gOtjgmC1M",
+    APP_CLIENT_ID: "63nq11n79gdh70occ3jisaeus8",
+    IDENTITY_POOL_ID: "us-west-2:802ec8e9-13ab-44ad-a0c6-6e175e698e86"
   }
 };
